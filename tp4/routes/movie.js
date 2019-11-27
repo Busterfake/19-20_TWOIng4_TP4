f2932f6d4b37ded972e83b4a560493c4c0336e6b
Busterfake <xelou1@hotmail.fr>
2019-11-27 13:52:45 +0100

    tp4 v2

--- tp4/routes/movie.js
+++ tp4/routes/movie.js
@@ -30,31 +30,30 @@ router.get('/:id', (req, res) => {
 router.put('/:title', (req, res) => {
     axios.get(`${api_url}?t=${req.params.title}&apikey=${api_key}`).then(function (response) {
         // on récupère la data de l'api
-        const movie = {
-            id: response.data.imdbID, 
-            movie: response.data.Title, 
-            yearOfRelease: response.data.Year, 
-            duration: response.data.Runtime,// en minutes 
-            actors: response.data.Actors,  
-            poster: response.data.Poster, // pochette du film
-            boxOffice: response.data.BoxOffice, // total des entrées (en $$$$)
-            rottenTomatoesScore: response.data.imdbRating //note du film
+            const movie = {
+                id: response.data.imdbID, 
+                movie: response.data.Title, 
+                yearOfRelease: response.data.Year, 
+                duration: response.data.Runtime,// en minutes 
+                actors: response.data.Actors,  
+                poster: response.data.Poster, // pochette du film
+                boxOffice: response.data.BoxOffice, // total des entrées (en $$$$)
+                rottenTomatoesScore: response.data.imdbRating //note du film
 
         };
         // documentation Axios
         // ajouter un film
         movies.push(movie);
         res.status(200).json({movies})
-    })
-        .catch(function (error) {
-            // handle error
-            console.log(error);
         })
-        .finally(function () {
-            // always executed
-        });
+            .catch(function (error) {
+                // handle error
+                console.log(error);
+            })
+            .finally(function () {
+                // always executed
+            });
 });
-
 
 /* on supprime un film par son ID */
 router.delete('/:id', (req, res) => {
@@ -70,22 +69,21 @@ router.delete('/:id', (req, res) => {
     });
 });
 
-
-/* UPDATE un film en renseignant son id */
+/* On modifie le nom d'un film grâce a son ID */
 router.post('/:id', (req, res) => {
-    // Récup l'id du film à modifier
-    const { id } = req.params;
-    // Récup l'info à update
-    const newNomMovie = req.body.movie;
-    console.log(req.body);
-    // trouve le film dans la database
-    const userToUpdate = _.find(movies, ["id", id]);
-    // Update
-    userToUpdate.movie = newNomMovie;
-    // Return message
-    res.json({
-        message: `Just updated ${id} with ${newNomMovie}`
-    });
+        // Récup l'id du film à modifier
+        const { id } = req.params;
+        // Récup l'info à update
+        const newNomMovie = req.body.movie;
+        console.log(req.body);
+        // trouve le film dans la database
+        const userToUpdate = _.find(movies, ["id", id]);
+        // Update
+        userToUpdate.movie = newNomMovie;
+        // Return message
+        res.json({
+            message: `Just updated ${id} with ${newNomMovie}`
+        });
 });
 
 module.exports = router;
